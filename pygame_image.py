b75e23752c740da76c9ac5e5a09ceec6d16ab10f
sfuuga <c0c2402890@edu.ten.ac.jp>
2025-04-15 15:47:37 +0900

    練習２前半

--- pygame_image.py
+++ pygame_image.py
@@ -10,14 +10,14 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.image.load("fig/3.png") #練習２前半
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img, [300, 200]) #練習4
         pg.display.update()
         tmr += 1        
         clock.tick(1)
